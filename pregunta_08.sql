55bb5ed44dcdcfd1df9b2ede64dd6dcd6df47cb0
carboledad <101777132+carboledad@users.noreply.github.com>
2022-04-14 15:22:58 -0500

    PREGUNTA8
    PREGUNTA8

--- pregunta_08.sql
+++ pregunta_08.sql
@@ -40,4 +40,4 @@
 --  3  2019  550.998571
 --
 --  >>> Escriba su codigo a partir de este punto <<<
-    SELECT CAST(SUBSTR(c23,1,4) AS INT) AS YEAR, ROUND(AVG(c21),6) FROM tbl2 GROUP BY SUBSTR(c23,1,4);
+    SELECT CAST(SUBSTR(c23,1,4) AS INT) AS YEAR, ROUND(AVG(c21),6) FROM tbl2 GROUP BY YEAR;

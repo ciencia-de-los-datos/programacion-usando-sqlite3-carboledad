b0347f7212d94716282585f8c4ea4c21be09947a
carboledad <101777132+carboledad@users.noreply.github.com>
2022-04-16 11:00:13 -0500

    Pregunta8_avg
    Pregunta8_avg

--- pregunta_08.sql
+++ pregunta_08.sql
@@ -40,4 +40,4 @@
 --  3  2019  550.998571
 --
 --  >>> Escriba su codigo a partir de este punto <<<
-    SELECT CAST(STRFTIME('%Y', c23) AS INT) AS YEAR, AVG(c21) FROM tbl2 GROUP BY YEAR;
+    SELECT CAST(STRFTIME('%Y', c23) AS INT) AS YEAR, avg(c21) FROM tbl2 GROUP BY YEAR;
